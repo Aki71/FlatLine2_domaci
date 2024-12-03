97b3f12ba8a4827f1226b66570f0dc980b06388f
Aki71 <cikaaljosa@gmail.com>
2024-12-03 20:02:33 +0100

    Dodatni fajlovi i sredjeni branchevi

--- FlatLine/obj/Release/net8.0/FlatLine.AssemblyInfo.cs
+++ FlatLine/obj/Release/net8.0/FlatLine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-FlatLine-37ff552f-e8c6-4be9-92fb-546af6b29c94")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlatLine")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5753c72bcf43662d0e0e9689575bd2e3c201114")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlatLine")]
