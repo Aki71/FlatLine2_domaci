d5068c3a5c9784f0cf18ba17d961ab675d35ac7c
Mladen <8mladen12cvetkovic06@gmail.com>
2024-12-04 12:18:30 +0100

    Prvi merge

--- FlatLine/obj/Debug/net8.0/FlatLine.AssemblyInfo.cs
+++ FlatLine/obj/Debug/net8.0/FlatLine.AssemblyInfo.cs
@@ -11,11 +11,12 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-FlatLine-37ff552f-e8c6-4be9-92fb-546af6b29c94")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlatLine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5753c72bcf43662d0e0e9689575bd2e3c201114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlatLine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlatLine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
